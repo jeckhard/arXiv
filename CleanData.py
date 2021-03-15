904de304640e4974ccbda3950c1e5447e46f3890
jeckhard <79570749+jeckhard@users.noreply.github.com>
2021-03-15 16:30:49 +0100

    Add files via upload

---
+++ CleanData.py
@@ -0,0 +1,113 @@
+"""
+Package to load in the arXiv metadata and clean it.
+Operations carried out:
+- load in the 'id', 'title' and 'categories' attributes from 'arxiv-metadata.json'
+- clean the categories to only contain 'cs', 'physics', 'math' and 'other'
+- add a label for each category and encode it into a boolean, 'other' is dropped
+- clean the title by removing new lines
+- write the data to 'arxiv-cleaned.csv'
+"""
+
+
+import json
+import pandas as pd
+
+def loadLinesjson(fileName,keys=[],numLines=None):
+    """
+    Loads in part of a .json file
+
+    :param fileName: str
+        path to the file
+    :param keys: list of str elements
+        list of attributes to be loaded in
+    :param numLines: int or None
+        number of lines to be loaded in
+        default=None, representing all lines
+    :return: pd dataframe
+        loaded in data
+    """
+
+    loaded = []
+
+    if not numLines:
+        numLines = sum(1 for _ in open(fileName))
+
+    with open(fileName, 'r') as file:
+        for i in range(numLines):
+            fullData = json.loads(file.readline())
+            selectedData = {}
+            for key in keys:
+                if key in fullData:
+                    selectedData[key] = fullData[key]
+                else:
+                    selectedData[key] = None
+            loaded.append(selectedData)
+        data = pd.DataFrame(loaded)
+
+    return data
+
+
+def cleanCategories(categories):
+    """
+    Cleans arXiv categories to 'cs', 'physics', 'math' and 'other'
+
+    :param categories: str
+        string containing all raw categories
+    :return: set of str elements
+        cleaned categories
+    """
+
+    categoryList = categories.split(" ")
+    physics = {"astro-ph", "cond-mat", "gr-qc", "hep-ex", "hep-lat", "hep-ph", "hep-th", "math-ph", "nlin", "nucl-ex","nucl-th", "physics", "quant-ph"}
+    labels = {"cs", "math", "physics"}
+
+
+    categorySet=set()
+
+    for category in categoryList:
+        if "." in category:
+            index = category.index(".")
+            category = category[:index]
+        if category in physics:
+            categorySet.add("physics")
+        elif category in labels:
+            categorySet.add(category)
+        else:
+            categorySet.add("other")
+
+    return categorySet
+
+
+def addCats(data,labels):
+    """
+    Add category labels to a data set
+
+    :param data: pd dataframe
+    :param labels: list of str elements
+        list of labels to be added
+    :return: pd dataframe
+    """
+    for label in labels:
+        data[label] = data["categories"].apply(lambda cat: int(label in cat))
+
+    return data
+
+
+def cleanTitles(title):
+    """
+    Remove new lines from a title
+
+    :param title: str
+    :return: str
+    """
+    title = title.replace("\n", " ")
+
+    return title
+
+
+data = loadLinesjson("arxiv-metadata.json",keys=["id","title","categories"])
+data["categories"] = data["categories"].apply(cleanCategories)
+data = addCats(data,{"cs", "math", "physics", "other"})
+data = data.drop(columns=["id","categories","other"])
+data["title"] = data["title"].apply(cleanTitles)
+data.to_csv("arxiv-cleaned.csv",index=False)
---
+++ EncodeData.py
@@ -0,0 +1,50 @@
+"""
+Package to encode arXiv titles using the universal sentence encoder
+"""
+
+import numpy as np
+import tensorflow_hub as hub
+import csv
+
+
+module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+embed = hub.load(module_url)
+
+def encodeTitle(df):
+    """
+    Encode the 'title' column of a dataframe using the universal sentence encoder
+
+    :param df: pd dataframe
+    :return: pd daraframe
+
+    """
+
+    df["encoder"] = df["title"].map(lambda title: np.array(embed([title])[0]))
+
+    for i in range(512):
+        df["encoder" + str(i)] = df["encoder"].map(lambda encoder: encoder[i])
+
+    return df
+
+encoderColumns = []
+for i in range(512):
+    encoderColumns.append("encoding" + str(i))
+categoryColumns = ["cs","physics","math"]
+
+
+with open("arxiv-cleaned.csv" , "r") as cleaned:
+    reader = csv.reader(cleaned)
+    next(reader,None)
+    with open("arxiv-encoded-full.csv", "w") as encoded:
+        writer = csv.writer(encoded)
+        writer.writerow(["title"] + encoderColumns + categoryColumns)
+        count = 0
+        for row in reader:
+            title = row[0]
+            categories = [row[1], row[2], row[3]]
+            encoding = np.array(embed([title])[0]).tolist()
+            toWrite = [title] + encoding + categories
+            writer.writerow(toWrite)
+            count += 1
+            if count % 1000 == 0:
+                print(count)
